20a85a64974462fa71fcfc9832ec31ee6cbf3502
SaulCGarciaAguilar <saulg606@gmail.com>
2024-03-20 11:37:35 -0500

    change getters and setters in ApiError, to Getter and Setter anotation in ApiError class

--- src/main/java/com/kalagan/kalagantest/infraestructure/adapter/web/exception/ApiError.java
+++ src/main/java/com/kalagan/kalagantest/infraestructure/adapter/web/exception/ApiError.java
@@ -1,11 +1,15 @@
 package com.kalagan.kalagantest.infraestructure.adapter.web.exception;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.http.HttpStatus;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Getter
+@Setter
 public class ApiError {
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd hh:mm:ss")
@@ -19,38 +23,6 @@ public class ApiError {
         this.timestamp = timestamp;
         this.status = status;
         this.message = message;
-        this.errors = errors;
-    }
-
-    public LocalDateTime getTimestamp() {
-        return timestamp;
-    }
-
-    public void setTimestamp(LocalDateTime timestamp) {
-        this.timestamp = timestamp;
-    }
-
-    public HttpStatus getStatus() {
-        return status;
-    }
-
-    public void setStatus(HttpStatus status) {
-        this.status = status;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public List<String> getErrors() {
-        return errors;
-    }
-
-    public void setErrors(List<String> errors) {
         this.errors = errors;
     }
 }
